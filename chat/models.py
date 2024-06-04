0079b73d590b6ee6c0c56874034b8fd170f1f2b3
Guilherme Otto <guilhermeotto22@gmail.com>
2024-06-04 02:08:16 -0300

    Alteração do model Room

--- chat/models.py
+++ chat/models.py
@@ -7,16 +7,16 @@ class Room(models.Model):
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200)
-    messages = models.ManyToManyField('Message')
+    messages = models.ManyToManyField('Message', blank=True)
     created_at = models.DateTimeField(auto_now_add = True)
 
     """A função ForeignKey na variável user é estabelecer uma relação entre a classe Room e o modelo de usuário definido em settings.AUTH_USER_MODEL. Isso significa que cada instância de Room estará associada a um único usuário
 
     O cascade faz com que se o usuário for excluído, todas as rooms associadas a esse usuário tambem serão excluídas.
 
-    O ManyToManyField faz com que um usuário possa estar associado aa muitas rooms e muitas rooms podem ter muitos usuários associados
+    O ManyToManyField faz com que um usuário possa estar associado aa muitas rooms e muitas rooms podem ter muitos usuários associados.
+    Por padrão ele vem com null=true, mas blank=false, tem que deixar como True para que vc possa criar uma room sem mensagem obrigatoria
 
-    A presença da relação ManyToManyField com o modelo Message sugere que uma Room pode estar associada a várias mensagens. Isso implica que, para que uma Room tenha sentido em termos de aplicação, é provável que seja desejável ter pelo menos uma mensagem associada a ela. É por isso que não dá pra fazer uma room sem uma mensagem ATÉ ENTÃO.
     """
 
 class Message(models.Model):
