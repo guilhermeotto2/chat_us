299473929a81c1b6dd10bad296d577293de5fb4f
Guilherme Otto <guilhermeotto22@gmail.com>
2024-06-06 20:46:58 -0300

    testando CRUD

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from . models import Message
 from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html', {})
+    messages = Message.objects.all()
+    return render(request, 'index.html', {'messages': messages})
