d29006a1d806b7a59de90433954cfb7e372a02b4
Guilherme Otto <guilhermeotto22@gmail.com>
2024-06-10 23:59:17 -0300

    renderizando o model Room pro frontend

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from . models import Message
+from . models import Message, Room
 from django.http import HttpResponse
 
+rooms = Room.objects.all()
 
 def home(request):
     messages = Message.objects.all()
-    return render(request, 'index.html', {'messages': messages})
+    return render(request, 'index.html', {
+        'messages': messages,
+        'rooms': rooms
+        })
