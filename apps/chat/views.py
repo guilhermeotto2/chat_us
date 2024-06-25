baf33a6f55277d4ee2a1ad9bdcb4771ae0144dce
Guilherme Otto <guilhermeotto22@gmail.com>
2024-06-25 01:52:56 -0300

    ajustando funções

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from . models import Message, Room
 from django.http import HttpResponse
 
-rooms = Room.objects.all()
 
 def home(request):
-    messages = Message.objects.all()
+    rooms = Room.objects.all()
     return render(request, 'chat/home.html', {
-        'messages': messages,
         'rooms': rooms
         })
