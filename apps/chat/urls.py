ee7b5085c3b4bd60ad93c5248de95b004a486bdc
Guilherme Otto <guilhermeotto22@gmail.com>
2024-06-25 02:52:49 -0300

    componentização dos templates

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("<pk>", views.RoomDetailView.as_view(), name="room_detail"),
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from . models import Message, Room
-from django.http import HttpResponse
-
+from django.views.generic.detail import DetailView
 
 def home(request):
     rooms = Room.objects.all()
     return render(request, 'chat/home.html', {
         'rooms': rooms
         })
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'chat/list_messages.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
